9104e2ba39b126ae56a669784b81537a325d4a2b
Alex Bryant <alexander@Gyarados.local>
2015-02-23 08:30:56 -0600

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Alex Bryant on 2/23/15.
+//  Copyright (c) 2015 Alex Bryant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Alex Bryant on 2/23/15.
+//  Copyright (c) 2015 Alex Bryant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
