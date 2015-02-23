e366921abea4a4a1a12640639bddfee58976aa59
Alex Bryant <alexander@Gyarados.local>
2015-02-23 10:24:34 -0600

    added a test class

---
+++ Git Test/ABTest.h
@@ -0,0 +1,13 @@
+//
+//  ABTest.h
+//  Git Test
+//
+//  Created by Alex Bryant on 2/23/15.
+//  Copyright (c) 2015 Alex Bryant. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ABTest : NSObject
+
+@end
---
+++ Git Test/ABTest.m
@@ -0,0 +1,13 @@
+//
+//  ABTest.m
+//  Git Test
+//
+//  Created by Alex Bryant on 2/23/15.
+//  Copyright (c) 2015 Alex Bryant. All rights reserved.
+//
+
+#import "ABTest.h"
+
+@implementation ABTest
+
+@end
